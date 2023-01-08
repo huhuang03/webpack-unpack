c2e72d0b62f7fedd8feb4944b5376dcfa55b7a4a
huhuang03 <huhuang03@gmail.com>
2023-01-08 15:58:33 +0800

    code style

--- bin.js
+++ bin.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-var path = require('path')
-var unpack = require('.')
-var argv = require('minimist')(process.argv.slice(2))
-var concat = require('simple-concat')
+const fs = require('fs')
+const path = require('path')
+const unpack = require('.')
+const argv = require('minimist')(process.argv.slice(2))
+const concat = require('simple-concat')
 
-var usage = 'webpack-unpack [--out outdir] < file.js'
+const usage = 'webpack-unpack [--out outdir] < file.js'
 
 if (argv.h || argv.help) {
   console.error(usage)
@@ -15,14 +15,14 @@ if (argv.h || argv.help) {
 
 concat(process.stdin, function (err, contents) {
   if (err) throw err
-  var modules = unpack(contents)
+  const modules = unpack(contents)
   if (!modules) {
     console.error('could not parse bundle')
     process.exit(1)
   }
 
   if (argv.out) {
-    var outdir = argv.out
+    const outdir = argv.out
     modules.forEach(function (row) {
       fs.writeFileSync(path.join(outdir, row.id + '.js'), row.source)
     })
