175c16aca0d31490533105c56912a176d5d54ffe
胡文凡 <wenfan.hu@infaith.com.cn>
2023-01-11 16:24:56 +0800

    test: change test to jest

---
+++ __tests__/index.js
@@ -0,0 +1,88 @@
+const unpack = require('../index')
+const webpack = require('webpack')
+const path = require('path')
+
+function buildFixture (name, cb) {
+  const config = path.join(__dirname, 'fixtures', name, 'webpack.config.js')
+  webpack(require(config)).run(function (err, stats) {
+    if (err) return cb(err)
+    // console.log('stats: ', stats)
+    cb(null, stats.compilation.assets['bundle.js'].source(), stats.compilation.assets)
+  })
+}
+
+// test('commonjs modules', function (t) {
+//   // t is the test.
+//   t.plan(4)
+//   buildFixture('commonjs', function (err, bundle) {
+//     t.ifError(err)
+//     // looks like modules is null?
+//     const modules = unpack(bundle)
+//     t.ok(modules)
+//     t.equal(modules.length, 4)
+//     t.ok(/^exports\.test=/.test(modules[3].source))
+//   })
+// })
+
+// test('provide only an ast', function (t) {
+//   t.plan(4)
+//   buildFixture('commonjs', function (err, bundle) {
+//     t.ifError(err)
+//     const ast = require('acorn').parse(bundle)
+//     const modules = unpack(ast)
+//     t.ok(modules)
+//     t.equal(modules.length, 4)
+//     // with a ( because it's a sequenceexpression
+//     t.ok(/^\(?exports\.test = /.test(modules[3].source))
+//   })
+// })
+
+test('es modules', done => {
+  buildFixture('esmodules', function (err, bundle) {
+    if (err) {
+      done(err)
+    }
+    expect(err).toBeNull()
+    // t.ifError(err)
+    console.log('err: ', err)
+    const modules = unpack(bundle)
+    console.log(modules)
+    done()
+    // t.ok(modules)
+    // t.equal(modules.length, 3) // just 3 because of ModuleConcatenation
+    // t.ok(/whatever/.test(modules[2].source))
+    // t.ok(/require\.r/.test(modules[2].source))
+    // t.ok(/require\.n/.test(modules[2].source))
+  })
+})
+//
+// test('entry points', function (t) {
+//   t.plan(5)
+//   buildFixture('entrypoints', function (err, bundle) {
+//     t.ifError(err)
+//     const modules = unpack(bundle)
+//     t.ok(modules)
+//     t.equal(modules.length, 3) // created a multi entry point proxy module
+//     const entry = modules.filter(function (m) { return m.entry })[0]
+//     t.ok(/require\(1\)/.test(entry.source))
+//     t.ok(/require\(2\)/.test(entry.source))
+//   })
+// })
+//
+// test('split bundles', function (t) {
+//   t.plan(8)
+//   buildFixture('split', function (err, bundle, assets) {
+//     t.ifError(err)
+//     const modules = unpack(bundle)
+//     const splitModules = unpack(assets['1.bundle.js'].source())
+//     t.ok(modules)
+//     t.ok(splitModules)
+//     t.equal(modules.length, 1)
+//     t.equal(splitModules.length, 1)
+//     const entry = modules.filter(function (m) { return m.entry })[0]
+//     const splitEntry = splitModules[0]
+//     t.ok(/require\.e/.test(entry.source))
+//     t.ok(/require\.t/.test(entry.source))
+//     t.ok(/console\.log/.test(splitEntry.source))
+//   })
+// })
